767c4a9e38a34ae779af798e9987e3f127457f8e
Alberto Suárez <alberto.suarez.ing@gmail.com>
2024-05-30 14:12:43 +0200

    Configuración del fichero de entorno
    He requerido la librería dotenv para configurar el fichero de entorno y he traído el puerto y lo he configurado para que escuche en el puerto 3000 por defecto. He lanzado la función on() para engancharme al evento de confirmación de servidor escuchando al puerto.

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 
-const htpp = require('node:http');
+const http = require('node:http');
 
 const app = require('./src/app');
 
+require('dotenv').config();
+
 
 const server = http.createServer(app);
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT);
+
+server.on('listening', () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
