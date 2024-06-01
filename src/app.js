05b6926ebeeb4cacd7ac1b356be19b014f5b7b4c
Alberto Suárez <alberto.suarez.ing@gmail.com>
2024-06-01 20:23:45 +0200

    Instalación de librería dayjs
    He instalado la librería dayjs para trabajar con un formato más claro de fechas, y he importado y establecido el formato de fechas dentro del fichero app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const dayjs = require('dayjs');
 
 const app = express();
+
+app.use((req, res, next) => {
+    console.log(dayjs().format('YYYY-MM-DD HH:mm:ss'));
+    next();
+});
 
 app.use('/api', require('./routes/api'));
 
