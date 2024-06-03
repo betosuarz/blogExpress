7cd8cc7b7969bf45b7e5309e2b7d68fc506dfcab
Alberto Suárez <alberto.suarez.ing@gmail.com>
2024-06-03 18:02:45 +0200

    Creación de middleware para registrar cronología de peticiones realizadas
    He creado un middleware para que registre automáticamente en el fichero main.log la cronología de las peticiones realizadas contra la base de datos

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dayjs = require('dayjs');
+const fs = require('node:fs/promises');
 
 const app = express();
 app.use(express.json());
@@ -8,6 +9,15 @@ app.use((req, res, next) => {
     const currentDate = dayjs().format('YYYY-MM-DD HH:mm:ss');
     console.log(currentDate);
     req.currentDate = currentDate;
+    next();
+});
+
+app.use(async (req, res, next) => {
+    const currentDate = dayjs().format('YYYY-MM-DD HH:mm:ss');
+    const data = `[${currentDate}] Método: ${req.method}. Url: ${req.url}\n`;
+
+    await fs.appendFile('main.log', data);
+
     next();
 });
 
